a0967f6eb3cbb52f99acd6e71938f09a6d6c51ff
zachee ishimwe <zacheeishimwe@gmail.com>
2026-03-16 01:16:41 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+
+import pandas as pd
+
+print("Arguments",sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f"Hello pipeline month={month}")
